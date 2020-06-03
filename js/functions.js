d056375dfe4d090bfd463b4d689a4446b01647c6
halcyonLife <il>
2020-06-03 13:37:10 +0800

    更新说

---
+++ js/functions.js
@@ -0,0 +1,68 @@
+/*
+ * http://love.hackerzhou.me
+ */
+
+// variables
+var $win = $(window);
+var clientWidth = $win.width();
+var clientHeight = $win.height();
+
+$(window).resize(function() {
+    var newWidth = $win.width();
+    var newHeight = $win.height();
+    if (newWidth != clientWidth && newHeight != clientHeight) {
+        location.replace(location);
+    }
+});
+
+(function($) {
+	$.fn.typewriter = function() {
+		this.each(function() {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			var timer = setInterval(function() {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length) {
+          $('#sakura').fadeIn(3000);
+          $('#text').css('display', 'none');
+          $('#clock-box').css('display', 'none');
+          $('.btnbg').css('text-shadow', '0px 0px 3px #ccc');
+          $('.btnbg').css('color', '#fbf5f5');
+          $('#clock-box a').css('color', '#fbf5f5');
+          $('#text').fadeIn();
+          $('#clock-box').fadeIn();
+					clearInterval(timer);
+				}
+			}, 66);
+		});
+		return this;
+	};
+})(jQuery);
+
+function timeElapse(date){
+	var current = Date();
+	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var days = Math.floor(seconds / (3600 * 24));
+	seconds = seconds % (3600 * 24);
+	var hours = Math.floor(seconds / 3600);
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	seconds = seconds % 3600;
+	var minutes = Math.floor(seconds / 60);
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	seconds = seconds % 60;
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	$("#clock").html(result);
+}
